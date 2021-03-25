f2298de6d9d1083cd4641c86416fb398b2d49d30
fabriciosouza21 <fabriciosouza21777@gmail.com>
2021-03-25 18:53:00 -0300

    prototipo da função de blast

--- bioinfo_project/bioinfo_web/urls.py
+++ bioinfo_project/bioinfo_web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf.urls.static import static
-
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name = 'home')
+    path('', views.home, name='home'),
+    path('dados/', views.get_dados_amostra, name='dados'),
 ]
--- bioinfo_project/bioinfo_web/views.py
+++ bioinfo_project/bioinfo_web/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
 from django.contrib import messages
+from .blast import main
+
 
 def home(request):
     return render(request, 'bioinfo_web/home.html')
+
+
+def get_dados_amostra(request):
+    if request.method == 'POST':
+        sequence = request.POST.get("sample_data")
+        f = open("bioinfo_web/myfile.fasta", "w")
+        f.writelines(sequence)
+        f.close()
+    main()
+    return HttpResponse('certo')
